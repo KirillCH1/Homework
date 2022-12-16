3a63de4c97d2dc896bae7ddd53c5e3d948a94123
Kirill <Kirik.77@Yandex.ru>
2022-12-16 15:31:02 +0300

    Первая задача Готова

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число a : ");
+int namber1 = int.Parse(Console.ReadLine() ??"");
+
+Console.Write("Введите число b : ");
+int namber2 = int.Parse(Console.ReadLine() ??"");
+
+if (namber1>namber2)
+{
+    Console.WriteLine($"a = {namber1} b = {namber2} -> max {namber1}");
+}
+else
+{
+    Console.WriteLine($"a = {namber1} b = {namber2} -> max {namber2}");
+}
+
