a3d3eb1c244c50af44c738530ebccbe9d7d1d880
Kirill <Kirik.77@Yandex.ru>
2022-12-16 16:11:51 +0300

    Выполнил вторую задачу

---
+++ Task 4/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Write("Введите первое число : ");
+int numberOne = int.Parse(Console.ReadLine() ??"");
+
+Console.Write("Введите второе число : ");
+int numberTwo = int.Parse(Console.ReadLine() ??"");
+
+Console.Write("Введите третье число : ");
+int numberThree = int.Parse(Console.ReadLine() ??"");
+
+if(numberOne > numberTwo)
+{
+    if(numberOne>numberThree)
+    {
+        Console.WriteLine($"{numberOne},{numberTwo},{numberThree} -> {numberOne}");
+    }
+    else
+    {
+        Console.WriteLine($"{numberOne},{numberTwo},{numberThree} -> {numberThree}");
+    }
+    
+}
+else
+{
+    if(numberTwo>numberThree)
+    {
+        Console.WriteLine($"{numberOne},{numberTwo},{numberThree} -> {numberTwo}");
+    }
+    else
+    {
+        Console.WriteLine($"{numberOne},{numberTwo},{numberThree} -> {numberThree}");
+    }
+}
+
