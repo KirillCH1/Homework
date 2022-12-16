20ae35125cd8246cb1de9828916334e96ed6244c
Kirill <Kirik.77@Yandex.ru>
2022-12-16 17:08:34 +0300

    Закончил 4е задание

--- Task 6/Program.cs
+++ Task 6/Program.cs
@@ -1,15 +1,18 @@
-﻿Console.Write("Введите число : ");
-int namber = int.Parse(Console.ReadLine() ??"");
-int result = namber %2;
+﻿
+// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Введите число : ");
+int number = int.Parse(Console.ReadLine() ??"");
+int result = number %2;
 if (result == 1)
 {
-    Console.Write($"{namber} -> нечетное число ");
+    Console.Write($"{number} -> нечетное число ");
 }
 else if(result == -1)
 {
-    Console.Write($"{namber} -> нечетное число ");
+    Console.Write($"{number} -> нечетное число ");
 }
 else
 {
-    Console.Write($"{namber} -> четное число ");
+    Console.Write($"{number} -> четное число ");
 }
