4b0b3cc980274248172523fe707b4fb739154c56
Kirill <Kirik.77@Yandex.ru>
2022-12-16 16:48:03 +0300

    Сделал 3е задание

---
+++ Task 6/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число : ");
+int namber = int.Parse(Console.ReadLine() ??"");
+int result = namber %2;
+if (result == 1)
+{
+    Console.Write($"{namber} -> нечетное число ");
+}
+else if(result == -1)
+{
+    Console.Write($"{namber} -> нечетное число ");
+}
+else
+{
+    Console.Write($"{namber} -> четное число ");
+}
