3a1cbf8376a2b1ef39543f5106fac5ef195458bb
Kirill <Kirik.77@Yandex.ru>
2022-12-24 13:45:14 +0300

    Сделал домашнее задание №10

---
+++ Task 10/Program.cs
@@ -0,0 +1,14 @@
+﻿
+// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. 
+
+int numb;
+while (true)
+{
+    Console.Write("Введите 3х значное число : ");
+    if (int.TryParse(Console.ReadLine(), out numb))
+        break;
+    Console.WriteLine("Ошибка ввода!");
+}
+int a = (numb / 10) % 10;
+
+Console.WriteLine($"{numb} -> {a}");
