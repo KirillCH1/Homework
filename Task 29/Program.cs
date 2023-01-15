76161955addc10171c5aafb5e5be7cc3707bb74b
Kirill <Kirik.77@Yandex.ru>
2023-01-15 13:48:44 +0300

    Домашнее задание 29 сделано

---
+++ Task 29/Program.cs
@@ -0,0 +1,38 @@
+﻿
+// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+Console.Clear();
+
+int[] array = new int[8];
+
+PrintArray(ArrayInitialized(array));
+
+void PrintArray(int[] array1)
+{
+    Console.Write("[");
+    for (int i = 0; i < array1.Length; i++)
+    {
+        Console.Write(array1[i]);
+        Console.Write(", ");
+    }
+    Console.Write("]");
+}
+int[] ArrayInitialized(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = GetNumberFromUser($"Введите {i + 1} число: ", "Ошибка Ввода");
+    }
+    return array;
+}
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
