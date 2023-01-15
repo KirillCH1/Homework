e9fd81a8b8d6ad48756da08eb3ac244e1188d896
Kirill <Kirik.77@Yandex.ru>
2023-01-15 12:44:50 +0300

    Сделал домашнее задание 27

---
+++ Task 27/Program.cs
@@ -0,0 +1,31 @@
+﻿
+//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Clear();
+int number = GetNumberFromUser("Введите целое число: ", "Ошибка Ввода");
+int sum = CalculateSumNumber(number);
+Console.WriteLine($"Cумма чисел равна: {sum}");
+
+int CalculateSumNumber(int number)
+{
+    int temp = number;
+    int sum = 0;
+    while (temp > 0)
+    {
+        sum += temp % 10;
+        temp /= 10;
+    }
+    return sum;
+}
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
