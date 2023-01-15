ad3b39503413211720f27bd342728bdd4ae3ffa0
Kirill <Kirik.77@Yandex.ru>
2023-01-15 12:50:25 +0300

    Исправил Дз

--- Task 27/Program.cs
+++ Task 27/Program.cs
@@ -8,12 +8,11 @@ Console.WriteLine($"Cумма чисел равна: {sum}");
 
 int CalculateSumNumber(int number)
 {
-    int temp = number;
     int sum = 0;
-    while (temp > 0)
+    while (number > 0)
     {
-        sum += temp % 10;
-        temp /= 10;
+        sum += number % 10;
+        number /= 10;
     }
     return sum;
 }
