efb24ad3c7074f8fe004cdc357d380cb87d03271
Kirill <Kirik.77@Yandex.ru>
2023-01-15 11:52:05 +0300

    Сделал дз 25

---
+++ Task 25/Program.cs
@@ -0,0 +1,33 @@
+﻿
+// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.Clear();
+
+int num = GetNumberFromUser("Введите целое число: ", "Ошибка Ввода");
+int power = GetNumberFromUser("Введите сепень числа: ", "Ошибка Ввода");
+int result = CalculatePowerNumber(num, power);
+Console.WriteLine($" Число {num} в степени {power} =  {result}");
+
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int CalculatePowerNumber(int number, int power)
+{
+    int res = number;
+    while (power - 1 > 0)
+    {
+        res *= number;
+        power--;
+    }
+    return res;
+}
