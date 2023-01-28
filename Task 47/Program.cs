8a3713ef226ed348b8ce5dacb5c246e5db37c393
Kirill <Kirik.77@Yandex.ru>
2023-01-28 10:59:20 +0300

    Сделала ДЗ 50

--- Task 47/Program.cs
+++ Task 47/Program.cs
@@ -9,28 +9,39 @@ int rows = int.Parse(Console.ReadLine() ?? "");
 Console.Write("Введите количество столбцов в массива: ");
 int collums = int.Parse(Console.ReadLine() ?? "");
 
-PrintArray(GetArray(rows, collums, 0, 100));
+PrintArray(GetArray(rows, collums, -100, 100));
 
-int[,] GetArray(int rows, int collums, int minV, int maxV)
+double[,] GetArray(int rows, int collums, int minV, int maxV)
 {
-    int[,] array = new int[rows, collums];
+    double[,] array = new double[rows, collums];
 
     for (int i = 0; i < rows; i++)
     {
         for (int j = 0; j < collums; j++)
         {
-            array[i, j] = new Random().Next(minV, maxV + 1);
+            array[i, j] = Convert.ToDouble(new Random().Next(minV, maxV)) / 10;
         }
     }
     return array;
 }
-void PrintArray(int[,] array)
+void PrintArray(double[,] array)
 {
+    string size;
+    string box;
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            Console.Write($"{array[i, j]} ");
+            box = Convert.ToString(array[i, j]);
+            if (box.Length == 1)
+                size = "    ";
+            else if (box.Length == 2)
+                size = "   ";
+            else if (box.Length == 3)
+                size = "  ";
+            else
+                size = " ";
+            Console.Write($"{size}" + $"{array[i, j]}  ");
         }
         Console.WriteLine();
     }
---
+++ Task 50/Program.cs
@@ -0,0 +1,63 @@
+﻿
+//Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Console.Clear();
+
+
+Console.Write("Введите строку массива: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите столбец массива: ");
+int columns = int.Parse(Console.ReadLine() ?? "");
+
+
+int[,] array = GetArray(5, 5, 0, 100);
+OutArray(array, rows, columns);
+
+void OutArray(int[,] array, int rows, int columns)
+{
+    if (array.GetLength(0) > rows && array.GetLength(1) > columns)
+    {
+        Console.Clear();
+        PrintArray(array);
+        Console.WriteLine($"  -> {array[rows, columns]}");
+    }
+
+    else
+    {
+        Console.Clear();
+        PrintArray(array);
+        Console.WriteLine($"  {rows} {columns} -> такого элемента нет");
+    }
+}
+
+int[,] GetArray(int rows, int columns, int minV, int maxV)
+{
+    int[,] array = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(minV, maxV);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    string box = "  ";
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] > 9)
+                box = "  ";
+            else
+                box = "   ";
+            Console.Write($"{box}" + $"{array[i, j]}");
+        }
+        Console.WriteLine();
+    }
+}
