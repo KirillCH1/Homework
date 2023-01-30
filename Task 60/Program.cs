8e3fc6388782b9c48813c895a052a10df2e65cdd
Kirill <Kirik.77@Yandex.ru>
2023-01-30 21:30:12 +0300

    Сделал ДЗ 60

---
+++ Task 60/Program.cs
@@ -0,0 +1,63 @@
+﻿
+// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+
+
+Console.Clear();
+
+PrintArray(GetArray(2, 2, 2));
+
+int[,,] GetArray(int x, int y, int z)
+{
+    Random rand = new Random();
+    int[,,] array = new int[x, y, z];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = rand.Next(0, 99);
+                CheckingMatches(array[i, j, k], i, j, k);
+            }
+        }
+    }
+    return array;
+    void CheckingMatches(int nambArray, int z, int x, int c)
+    {
+        for (int i = 0; i < z; i++)
+        {
+            for (int j = 0; j < x; j++)
+            {
+                for (int k = 0; k < c; k++)
+                {
+                    if (nambArray == array[i, j, k])
+                        array[i, j, k] = rand.Next(0, 99);
+                }
+            }
+        }
+    }
+}
+
+void PrintArray(int[,,] array)
+{
+    string box = " ";
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+
+                if (array[i, j, k] > 9)
+                    box = "";
+                else
+                    box = " ";
+                Console.Write(box + $" {array[i, j, k]}({i},{j},{k}) ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+    Console.WriteLine();
+}
--- Task 62/Program.cs
+++ Task 62/Program.cs
@@ -3,49 +3,11 @@
 
 Console.Clear();
 
-
 int[,] array = new int[4, 4];
-PrintArray(SpiraFilling1(array));
 
+PrintArray(SpiraFilling(array));
 
 int[,] SpiraFilling(int[,] array)
-{
-    int positionNumber = array[0, 0];
-    int counter = 1;
-    int line = array.GetLength(0);
-    int column = array.GetLength(1);
-    for (int j = 1; j < line * column + 1; j++)
-    {
-        for (int i = j - 1; i < line; i++)
-        {
-            array[j - 1, i] = counter;
-            counter++;
-        }
-        column = column - 1;
-        for (int i = j; i < column + 1; i++)
-        {
-            array[i, column] = counter;
-            counter++;
-        }
-        line = line - 1;
-        for (int i = line - 1; i > -1; i--)
-        {
-            array[line, i] = counter;
-            counter++;
-        }
-        column = column - 1;
-        for (int i = column; i > 0; i--)
-        {
-            array[i, j - 1] = counter;
-            counter++;
-        }
-        line = line - 1;
-    }
-    return array;
-
-}
-
-int[,] SpiraFilling1(int[,] array)
 {
     int line = array.GetLength(0);
     int column = array.GetLength(1);
@@ -68,7 +30,7 @@ int[,] SpiraFilling1(int[,] array)
         }
         line--;
         column--;
-        for (int i = line-1; i > start; i--) // влево
+        for (int i = line - 1; i > start; i--) // влево
         {
             array[line, i] = currentValue;
             currentValue++;
