a394b772c33fe05099e8d39c256e3f59feea47ac
Kirill <Kirik.77@Yandex.ru>
2023-02-02 19:42:33 +0300

    Сделал ДЗ 64 и 66

---
+++ Task 64/Program.cs
@@ -0,0 +1,22 @@
+﻿
+//Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int numbers = int.Parse(Console.ReadLine() ?? "");
+Console.Write($" N = {numbers} -> \"");
+OutputNaturalNumbers(numbers);
+
+
+void OutputNaturalNumbers(int numbers)
+{
+    Console.Write($" {numbers},");
+    numbers--;
+    if (numbers == 1)
+    {
+        Console.Write($" {numbers}\"");
+        return;
+    }
+    OutputNaturalNumbers(numbers);
+}
---
+++ Task 66/Program.cs
@@ -0,0 +1,33 @@
+﻿
+// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.Clear();
+
+int numbM = EnteringNumbers("M");
+int numbN = EnteringNumbers("N");
+int result = 0;
+Console.Write($"M = {numbM}; N = {numbN} ->");
+OutputNaturalNumbers(numbM, numbN, result);
+
+
+
+void OutputNaturalNumbers(int start, int end, int temp)
+{
+    temp = temp + start;
+    start++;
+
+    if (start == end)
+    {
+        temp = temp + start;
+        Console.Write($" {temp}");
+        return;
+    }
+    OutputNaturalNumbers(start, end, temp);
+}
+
+int EnteringNumbers(string messege)
+{
+    Console.Write($"Введите число \"{messege}\": ");
+    int numbers = int.Parse(Console.ReadLine() ?? "");
+    return numbers;
+}
